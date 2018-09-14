c1fdf6a52c51bf181e1dd20efd5e4046c5c0e51d
Res Yeg <shaqk702@gmail.com>
2018-09-13 22:31:26 -0700

    clarified testing libraries

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -60,8 +60,16 @@ const AboutPage = (props, context) => {
               <li>Server Side -
                 <span> <a href="http://expressjs.com/" rel="noopener noreferrer" target="_blank">Express</a>, <a href="https://nodejs.org/en/" target="blank" rel="noopener noreferrer">Node</a>, <a href="http://mongoosejs.com/" target="blank" rel="noopener noreferrer">Mongoose</a>, <a href="https://www.mongodb.com/" target="blank" rel="noopener noreferrer">Mongo</a>, <a href="http://docs.sequelizejs.com/" target="blank" rel="noopener noreferrer">Sequelize</a>, <a href="https://www.python.org/" target="blank" rel="noopener noreferrer">Python</a>, <a href="https://www.djangoproject.com/" target="blank" rel="noopener noreferrer">Django</a>, <a href="https://www.sqlite.org/index.html" target="blank" rel="noopener noreferrer">SQLite</a>, <a href="http://www.django-rest-framework.org/" target="blank" rel="noopener noreferrer">Django REST Framework</a>, <a href="https://graphql.org/" target="blank" rel="noopener noreferrer">GraphQL</a> - <a href="https://www.howtographql.com/" target="blank" rel="noopener noreferrer">Tutorial</a></span>
               </li>
-              <li>Testing -
-                <span> <a href="https://facebook.github.io/jest/" target="blank" rel="noopener noreferrer">Jest</a>, <a href="https://github.com/airbnb/enzyme" target="blank" rel="noopener noreferrer">Enzyme</a>, <a href="https://mochajs.org/" target="blank" rel="noopener noreferrer">Mocha</a>, <a href="http://www.chaijs.com/" target="blank" rel="noopener noreferrer">Chai</a>, <a href="http://sinonjs.org/" target="blank" rel="noopener noreferrer">Sinon</a></span>
+              <li>Testing JS -
+                <span> 
+                  <a href="https://facebook.github.io/jest/" target="blank" rel="noopener noreferrer">Jest-<i>testrunner/assertions/display&watch/snapshots/mocks&spies&stubs/coverage</i></a>,&nbsp;
+                  <a href="https://jasmine.github.io/" target="blank" rel="noopener noreferrer">Jasmine-<i>testrunner/assertions/display&watch/mocks&spies&stubs</i></a>,&nbsp;
+                  <a href="https://mochajs.org/" target="blank" rel="noopener noreferrer">Mocha-<i>testrunner/display&watch</i></a>,&nbsp;
+                  <a href="https://github.com/visionmedia/supertest" target="blank" rel="noopener noreferrer">Supertest-<i>NodeWebAPI</i></a>,&nbsp;
+                  <a href="https://github.com/airbnb/enzyme" target="blank" rel="noopener noreferrer">Enzyme-<i>React/mocks&spies&stubs</i></a>,&nbsp;
+                  <a href="http://www.chaijs.com/" target="blank" rel="noopener noreferrer">Chai-<i>assertions</i></a>,&nbsp;
+                  <a href="http://sinonjs.org/" target="blank" rel="noopener noreferrer">Sinon-<i>mocks&spies&stubs</i></a>
+                </span>
               </li>
               <li>Deployment -
                 <span> <a href="https://firebase.google.com/" target="blank" rel="noopener noreferrer">Firebase</a>, <a href="https://www.mongodb.com/cloud" target="blank" rel="noopener noreferrer">MongoDB Atlas</a>, <a href="https://mlab.com/" target="blank" rel="noopener noreferrer">mLab</a>, <a href="https://pages.github.com/" target="blank" rel="noopener noreferrer">Github Pages</a>, <a href="https://www.netlify.com/" target="blank" rel="noopener noreferrer">Netlify</a>, <a href="https://www.heroku.com/" target="blank" rel="noopener noreferrer">Heroku</a>, <a href="https://azure.microsoft.com/en-us/" target="blank" rel="noopener noreferrer">Microsoft Azure</a>, <a href="https://aws.amazon.com/" target="blank" rel="noopener noreferrer">AWS</a>, <a href="https://mlab.com/" target="blank" rel="noopener noreferrer">mLab</a></span>
